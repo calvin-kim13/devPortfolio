de3beda684a5d6c72ff38ce509417f9a99ee4d0f
Calvin Kim <calvinkim1230@gmail.com>
2022-03-01 19:23:44 -0800

    updated css styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ const fullname = document.getElementById('name');
 const email = document.getElementById('email');
 const subject = document.getElementById('subject');
 const message = document.getElementById('message');
+const body = document.querySelector('body');
+const checkbox = document.querySelector('.checkbtn')
 
 sendMail = () => {
     const link = `mailto:calvinkim1230@gmail.com?subject=${fullname.value} (${email.value}): "${subject.value}"&body=${message.value}`;
