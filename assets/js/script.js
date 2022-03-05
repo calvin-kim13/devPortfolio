ebac299ba5010cc0cdccade29828801ced0e1a27
Calvin Kim <calvinkim1230@gmail.com>
2022-03-04 17:14:38 -0800

    updated styling to nav bar and added dark mode feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+// DARK MODE
+const body = document.querySelector('body'),
+    sidebar = body.querySelector('.sidebar'),
+    toggle = body.querySelector('.toggle'),
+    modeSwitch = body.querySelector('.toggle-switch'),
+    modeText = body.querySelector('.mode-text');
+
+toggle.addEventListener('click', () => {
+    sidebar.classList.toggle('close')
+})
+
+modeSwitch.addEventListener('click', () => {
+    body.classList.toggle('dark');
+
+    if (body.classList.contains('dark')) {
+        modeText.innerText = "Light Mode"
+    } else {
+        modeText.innerText = "Dark Mode"
+    }
+})
+
 // CONTACT MAIL
 const fullname = document.getElementById('name');
 const email = document.getElementById('email');
